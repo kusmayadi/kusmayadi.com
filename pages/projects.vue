1cf9b0301edf20ddee77a35e6ded2bd6f0ad9919
Kusmayadi <kusmayadi@gmail.com>
2020-04-02 14:25:46 +0700

    Add projects page

---
+++ pages/projects.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="flex justify-center bg-white py-10 px-6 content text-sm sm:text-base md:text-base">
+    <div>
+      <p class="italic pb-6 text-xs sm:text-sm text-gray-600">* Please note that some links might be dead or have changed.</p>
+
+      <div class="mb-6">
+        <h1>restock.id — 2019 - 2020</h1>
+        <h3><a href="https://restock.id">restock.id</a></h3>
+        <p>Build website & dashboard for restock.id for managing lender & borrower, request loan, apply as borrower & integrating with third parties.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 7, Laravel 6, MySQL</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>bro.do — 2016 - 2019</h1>
+        <h3><a href="https://bro.do">bro.do</a></h3>
+        <p>Build ecommerce system & POS for bro.do, including integration with warehouse system &amp; payment gateway, system architecture, and deployment scripts.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Laravel 6, MySQL, React, Go, AWS, ansible</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>brambang.com — 2016</h1>
+        <h3><a href="https://brambang.com">brambang.com</a></h3>
+        <p>Build ecommerce system for brambang.com, including system architecture, deployment scripts.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> Node.js, sails.js, Angular, ansible, MySQL, AWS</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Pikmiap — 2016</h1>
+        <h3><a href="https://pikmiap.com">pikmiap.com</a></h3>
+        <p>Build dashboard for ordering courier delivery & managing order, including integrated with third parties.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Laravel 5, MySQL, jQuery, DigitalOcean</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>LV8.tv — 2013 - 2016</h1>
+        <h3><a href="https://lv8.tv">lv8.tv</a></h3>
+        <p>Build video platform system. Develop API, video transcoder.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Laravel 4, MySQL, Node.js, sails.js, video.js, AWS, CDN</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Liputan6 — 2013</h1>
+        <h3><a href="https://liputan6.com/">liputan6.com</a></h3>
+        <p>Build new CMS system for liputan6.com.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Laravel 4, Selenium, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Multiply — 2012 - 2013</h1>
+        <h3><a href="https://multiply.com/">multiply.com</a></h3>
+        <p>Build e-commerce system.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Symfony, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Beautique Clinic — 2012</h1>
+        <h3><a href="http://beautiqueclinic.com/">beautiqueclinic.com</a></h3>
+        <p>Developed website for Beautique Clinic.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.2.0, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Convrrt — 2012</h1>
+        <h3><a href="http://mobileconvrrt.com/">mobileconvrrt.com</a></h3>
+        <p>Add some functionalities to existing sites, including: integration with Facebook, Twitter & Pinterest. Fixed some bugs on existing site.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.2.0, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Earthway — 2012</h1>
+        <h3><a href="http://www.earthway.com">earthway.com</a></h3>
+        <p>Customized Vendo e-commerce system.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.2.0, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Las Cholas En Japon — 2011</h1>
+        <h3><a href="http://www.lascholasenjapon.com">lascholasenjapon.com</a></h3>
+        <p>Website development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.2.0, MySQL, jQuery</p>
+      </div>
+      <div class="mb-6">
+        <h1>Bali Plus for Ipad — 2011</h1>
+        <h3><a href="http://itunes.apple.com/us/app/bali-plus/id439210837">Apple App Store</a></h3>
+        <p>iPad app development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> Appcelerator Titanium, Javascript</p>
+      </div>
+      <div class="mb-6">
+        <h1>Neokurio — 2011</h1>
+        <h3><a href="http://www.neokurio.com">neokurio.com</a></h3>
+        <p>Website development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.1.3.1, MySQL, jQuery, Apache</p>
+      </div>
+      <div class="mb-6">
+        <h1>LiveUnsigned — 2010-2011</h1>
+        <h3><a href="http://www.liveunsigned.com">liveunsigned.com</a></h3>
+        <p>Improvement & enhancement on the existing system, including fixing some bugs.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3.4, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Wisata Dewata — 2010-2011</h1>
+        <h3><a href="http://www.wisatadewata.com">wisatadewata.com</a></h3>
+        <p>Web applications development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 3.0.7, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Facebook Pages — 2010</h1>
+        <p>PSD to Facebook Pages development</p>
+        <p>
+          Pages:
+          <ul>
+            <li><a href="http://www.facebook.com/pages/True-Image-Photography/110464739660">True Image Photography</a></li>
+            <li><a href="http://www.facebook.com/pages/American-Air-Campers-Association/108592692518343">American Air Campers Association</a></li>
+            <li><a href="http://www.facebook.com/keaneyevideo?sk=app_7146470109">Kean Eye Video Services</a></li>
+            <li><a href="http://www.facebook.com/capecoralkarate?v=app_4949752878">Cape Coral Isshinryu Karate Academy</a></li>
+            <li><a href="http://www.facebook.com/Stella?sk=app_169397313078933">Stella Artois - Black, UK</a>
+            <li><a href="http://www.facebook.com/pages/Meridian-ID/Harmon-Chiropractic/235548913945">Harmon Chiropractic</a></li>
+            <li><a href="http://www.facebook.com/kemanaonlinestore?v=app_10442206389">kemana.com - Di Mana Kamu? v.02 Video Upload Competition</a></li>
+            <li><a href="http://www.facebook.com/gossipfusion?v=app_4949752878">Gossip Fusion - Our Menu</a></li>
+          </ul>
+        </p>
+        <p class="tech-stack"><span>Tech Stack:</span> HTML, CSS, FBML, FBJS</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Sportowefakty Admin Page — 2010</h1>
+        <h3><a href="http://www.sportowefakty.pl">sportowefakty.pl</a></h3>
+        <p>Web applications development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3.4, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Parliament Hill Website — 2010</h1>
+        <h3><a href="http://www.parliamenthillmusic.com">parliamenthillmusic.com</a></h3>
+        <p>Website development, including WordPress Theme customization and plugin development.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP, WordPress, MySQL, jQuery</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Kemana.Com — 2009-2010</h1>
+        <h3><a href="http://www.kemana.com">kemana.com</a></h3>
+        <p>Magento customization development, including Magento custom modules development. PSD to HTML/CSS design conversion.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Magento, jQuery, Apache</p>
+      </div>
+      <div class="mb-6">
+        <h1>Deefusion — 2009</h1>
+        <h3><a href="http://www.deefusion.com">deefusion.com</a></h3>
+        <p>Web applications development. PSD to HTML/CSS design conversion.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3.4, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Think Math Tutoring — 2009</h1>
+        <h3><a href="http://www.thinkmathtutoring.com">thinkmathtutoring.com</a></h3>
+        <p>Web applications development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3.4, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Baru Di Bali — 2009</h1>
+        <h3><a href="http://www.barudibali.com">barudibali.com</a></h3>
+        <p>Web applications development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, CakePHP, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Out Of Limits Website — 2009</h1>
+        <h3><a href="http://www.outoflimits-bali.com">outoflimits-bali.com</a></h3>
+        <p>Website development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3, MySQL, jQuery, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>24jam.com Website — 2008-2009</h1>
+        <h3><a href="http://www.24jam.com">24jam.com</a></h3>
+        <p>Web applications development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, Kohana 2.3, MySQL, jQuery, Bluetrip, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Prep Channel — 2007-2008</h1>
+        <h3><a href="http://www.prepchannel.com">prepchannel.com</a></h3>
+        <p>Web applications development & modifications and customizing SocialEngine.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP, SocialEngine, Smarty, MySQL, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Ciranjang Hospital Website — 2007</h1>
+        <h3><a href="http://www.rsciranjang.com">rsciranjang.com</a></h3>
+        <p>Content Management & website development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 5, CodeIgniter, MySQL, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>mobil46.Com Website — 2007</h1>
+        <h3><a href="http://www.mobil46.com">mobil46.com</a></h3>
+        <p>Forum & Advertising Banner Module development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 4, MySQL, Apache, SVN</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Dik’s Photography Website — 2006</h1>
+        <h3><a href="http://www.diksphoto.com">diksphoto.com</a></h3>
+        <p>Content Management System & website development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 4, MySQL, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Multimedia Download for mobile3845.com — 2006</h1>
+        <h3><a href="http://www.mobile3845.com">mobile3845.com</a></h3>
+        <p>Multimedia mobile content (ringtones, wallpaper, themes, etc) application development.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 4, PostgreSQL, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Saywire — 2005-2009</h1>
+        <h3><a href="http://www.saywire.com">saywire.com</a></h3>
+        <p>Social networking applications development, including: image gallery, enotes, guestbook, discussions, blog, etc.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 4, MySQL, Apache, SVN, jQuery</p>
+      </div>
+      <div class="mb-6">
+        <h1>Direktorat Jendral Olahraga — 2004</h1>
+        <h3><a href="http://www.ditjora.web.id">www.ditjora.web.id</a></h3>
+        <p>Content Management System & Website development.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 3, MySQL, Apache</p>
+      </div>
+      <div class="mb-6">
+        <h1>Jagat Media SMS — 2003</h1>
+        <p>Content Management System & mobile applications (text/sms based) development, including: magazine subscriptions, ad placement, quiz, etc.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 3, MySQL, Apache, Kannel</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Artefactor Website — 2003</h1>
+        <p>Content Management System & Website development.</p>
+        <p class="tech-stack"><span>Tech Stack:</span> PHP 4, Fusebox 3, MySQL, Apache</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Detik.com Mobile Content Application (Text/Sms Based) — 2001-2004</h1>
+        <h3><a href="https://detik.com">detik.com</a></h3>
+        <p>
+          <ul>
+            <li>Content Management System development for updating SMS content</li>
+            <li>Application gateway development to manage SMS request passed by cellular operator through HTTP & SMPP protocols</li>
+            <li>Mobile content applications development, including: news, polling, chat, astrology, ringtones, etc.</li>
+          </ul>
+        </p>
+        <p class="tech-stack">
+          <span>Tech Stack:</span>
+          <ul class="list-disc list-inside">
+            <li>First version: PHP 4, Fusebox 3, MySQL, Apache, Kannel</li>
+            <li>Latest version: PHP 4, Fusebox 4, MySQL, Apache, Kannel</li>
+          </ul>
+        </p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Detik.com WBNS a.k.a Jahex (Web Based News Station) — 1999-2001</h1>
+        <h3><a href="https://detik.com">detik.com</a></h3>
+        <p>Content Management System &amp; web application development for detik.com, including: news, polling, forum, astrology, membership, etc.</p>
+        <p class="tech-stack">
+          <span>Tech Stack:</span>
+          <ul>
+            <li>First version: ColdFusion, Fusebox 3, MSQL, IIS Server</li>
+            <li>Latest version: PHP 4, Fusebox 4, MySQL, Apache</li>
+          </ul>
+        </p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Editorial Work Flow System For Intisari Magazine (Intranet) — 1996</h1>
+        <p>Editorial Work Flow System for Intisari Magazine development</p>
+        <p class="tech-stack"><span>Tech Stack:</span> ColdFusion, IIS Server, MS Access</p>
+      </div>
+
+      <div class="mb-6">
+        <h1>Static Website Projects — 1995-2000</h1>
+        <p>
+          Static page websites development.
+          <ul>
+            <li>Bank Jaya, <a href="http://www.bankjaya.com">http://www.bankjaya.com</a></li>
+            <li>PT Timah, <a href="http://www.pttimah.com">http://www.pttimah.com</a></li>
+            <li>Tabloid Bintang Indonesia, <a href="http://www.bintang.com">http://www.bintang.com</a></li>
+          </ul>
+          </p>
+        <p class="tech-stack"><span>Tech stack:</span> HTML</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Projects'
+    },
+
+    created() {
+      this.$store.commit('changeHeaderTitle', 'Projects');
+      this.$store.commit('isHomepage', false);
+    },
+
+    mounted () {
+      this.$store.commit('isMenuOpen', false);
+    }
+  }
+</script>
