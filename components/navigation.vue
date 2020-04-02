1aa84c5ac483546527387876593e428194b37396
Kusmayadi <kusmayadi@gmail.com>
2020-04-02 15:09:28 +0700

    Add contact page

--- components/navigation.vue
+++ components/navigation.vue
@@ -16,8 +16,8 @@
     <div :class="$store.state.isMenuOpen ? 'block' : 'hidden'" class="px-4 bg-gray-800 py-4 opacity-75 sm:flex sm:bg-transparent text-sm">
       <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="/work-experience"><font-awesome-icon :icon="['fas', 'briefcase']" class="mr-1"/> Work Experience</nuxt-link>
       <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="/projects"><font-awesome-icon :icon="['fas', 'archive']" class="mr-1"/> Projects</nuxt-link>
-      <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="open-source"><font-awesome-icon :icon="['fas', 'code']" class="mr-1"/> Open Source</nuxt-link>
-      <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="#"><font-awesome-icon :icon="['far', 'envelope']" class="mr-1"/> Contact</nuxt-link>
+      <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="/open-source"><font-awesome-icon :icon="['fas', 'code']" class="mr-1"/> Open Source</nuxt-link>
+      <nuxt-link class="block sm:inline-block sm:ml-2 px-2 py-1 font-semibold hover:text-white focus:text-white" to="/contact"><font-awesome-icon :icon="['far', 'envelope']" class="mr-1"/> Contact</nuxt-link>
     </div>
   </header>
 </template>
---
+++ pages/contact.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center items-center bg-white py-10 px-6">
+    <div>
+      <p class="text-center">You can contact me through my social media channel:</p>
+
+      <div class="flex justify-center text-sm sm:text-4xl py-4">
+        <div class="px-2 sm:px-4">
+          <a href="https://www.linkedin.com/in/kusmayadi/" target="linkedin"><font-awesome-icon :icon="['fab', 'linkedin-in']"/> LinkedIn</a>
+        </div>
+
+        <div class="px-2 sm:px-4">
+          <a href="https://github.com/kusmayadi" target="github"><font-awesome-icon :icon="['fab', 'github']"/> Github</a>
+        </div>
+
+        <div class="px-2 sm:px-4">
+          <a href="https://twitter.com/kusmayadi" target="twitter"><font-awesome-icon :icon="['fab', 'twitter']"/> Twitter</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Contact Me'
+    },
+
+    created() {
+      this.$store.commit('changeHeaderTitle', 'Keep in Touch');
+      this.$store.commit('isHomepage', false);
+    },
+
+    mounted () {
+      this.$store.commit('isMenuOpen', false);
+    }
+  }
+</script>
