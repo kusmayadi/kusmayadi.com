53118df5a1aa165e8daa24d408e3c693873f063d
Kusmayadi <kusmayadi@gmail.com>
2020-04-27 15:57:13 +0700

    Update open source page, add link to artisan user packagist

--- pages/open-source.vue
+++ pages/open-source.vue
@@ -2,11 +2,12 @@
   <div class="flex justify-center bg-white py-10 px-6 content">
     <div>
       <div class="mb-6">
-        <h1>Artisan User Management</h1>
+        <h1>Artisan User</h1>
+        <h3><font-awesome-icon :icon="['fas', 'globe']" class="text-gray-500" /> <a target="packagist" href="https://packagist.org/packages/kusmayadi/artisan-user">artisan user on packagist</a></h3>
         <h3><font-awesome-icon :icon="['fab', 'github']" class="text-gray-500" /> <a target="github" href="https://github.com/kusmayadi/artisan-user-management">Source Code</a></h3>
         <p>Laravel package for managing users through artisan console.</p>
         <p class="tech-stack"><span>Language:</span> PHP</p>
-        <p class="tech-stack"><span>Framework:</span> Laravel 6</p>
+        <p class="tech-stack"><span>Framework:</span> Laravel</p>
       </div>
 
       <div class="mb-6">
